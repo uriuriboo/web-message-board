b505326361be175368a1b84a8a9e5f0269b3f856
uriuribo <rokkouribou@gmail.com>
2023-01-07 19:13:35 +0900

    ひな形を生成

--- public/index.php
+++ public/index.php
@@ -1,2 +1,27 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title>ひとこと掲示板</title>
+    <link rel="stylesheet" href="./assets/main.css" />
+</head>
+
+<body>
+    <div class="page-cover">
+        <p class="page-title">ひとこと掲示板</p>
+        <div class="form-cover">
+            <!-- 投稿内容入力フォーム -->
+        </div>
+        <hr class="page-divider" />
+            <div class="message-list-cover">
+            <!-- 投稿された内容のリストを表示 -->
+            </div>
+            <hr class="page-divider" />
+    </div>
+</body>
+
+</html>
